c565a1e84b7fe47bcec2f58e5e79de8600a52902
marfur9 <marfur9@gmail.com>
2022-12-03 20:55:00 +0100

    Day 3 done

--- Day3/code.py
+++ Day3/code.py
@@ -1,3 +1,27 @@
-inputfile = open('Day?/example.txt', 'r')
+import time
+start_time = time.time()
+
+def splitstring(value):
+    string1, string2 = value[:len(value)//2], value[len(value)//2:]
+    return string1, string2
+
+inputfile = open('Day3/example.txt', 'r')
+sum=0
 for line in inputfile:
-        print(line)
+        splitted=splitstring(line)
+        comp1=splitted[0]
+        comp2=splitted[1]
+        same=""
+        for item1 in comp1:
+                for item2 in comp2:
+                        if item1==item2:
+                                same=item1
+                                break
+        letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+        for count, letter in enumerate(letters, start=1):
+                if same==letter:
+                        sum=sum+count
+                        break
+print (sum)
+
+print("Process finished --- %s seconds ---" % (time.time() - start_time))
--- Day3/code2.py
+++ Day3/code2.py
@@ -1,3 +1,38 @@
-inputfile = open('Day?/example.txt', 'r')
+
+inputfile = open('Day3/input.txt', 'r')
+sum=0
+letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+i=0
+group=[]
 for line in inputfile:
-        print(line)
+        
+        if i<3:
+                group.append(line)
+                print(line + " added")
+                i=i+1
+
+        if i==3:
+                member1,member2,member3=group[0],group[1],group[2]
+                same=""
+                for letter1 in member1:
+                        for letter2 in member2:
+                                for letter3 in member3:
+                                        if letter1==letter2 and letter1==letter3:
+                                                same=letter1
+                                                print("found letter: " + letter1)
+                                                break
+                                else:
+                                        continue
+                                break
+                        else:
+                                continue
+                        break
+                
+                for count, letter in enumerate(letters, start=1):
+                        if same==letter:
+                                sum=sum+count
+                i=0
+                group=[]
+
+        
+print (sum)
