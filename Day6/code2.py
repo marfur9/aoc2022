ba8b18c013bd06cf3eac219ced74b08eb989f555
marfur9 <marfur9@gmail.com>
2022-12-06 10:46:40 +0100

    Day 6 complete

--- Day6/code2.py
+++ Day6/code2.py
@@ -1,4 +1,34 @@
+def findDuplicates(chars):
+        seen = set()
+
+
+        for x in chars:
+                if x in seen:
+                        return False
+                else:
+                        seen.add(x)
+
+        return True
+
+
+
 inputfile = open('Day6/example.txt', 'r')
-#inputfile = open('Day6/input.txt', 'r')
+inputfile = open('Day6/input.txt', 'r')
 for line in inputfile:
-        print(line)
+        chars=[]
+        sum=0
+        for char in line:
+                sum=sum+1
+                if len(chars) < 14:
+                        chars.append(char)
+                        if len(chars)==4:
+                                if findDuplicates(chars):
+                                        break
+                else:
+                        chars.reverse()
+                        chars.pop()
+                        chars.reverse()
+                        chars.append(char)
+                        if findDuplicates(chars):
+                                break
+        print(sum)
--- Day7/code2.py
+++ Day7/code2.py
@@ -1,3 +1,4 @@
-inputfile = open('Day?/example.txt', 'r')
+inputfile = open('Day7/example.txt', 'r')
+#inputfile = open('Day7/input.txt', 'r')
 for line in inputfile:
         print(line)
